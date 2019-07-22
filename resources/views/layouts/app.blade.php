1f6f23ee469a51ede0ae9178a9d5389fcb9f703b
Ivanda Nothabeer <kossaw.eve@gmail.com>
2019-07-23 00:12:46 +1200

    Fixed front page Waypoint text"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,10 +65,10 @@
                     <li class="nav-item"><a class="nav-link" href="{{ url('/members')}}">Members</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ url('/tracking')}}">Tracking</a></li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Turnpoints</a>
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Waypoints</a>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" href="{{ url('/cups')}}">Download Turnpoints</a>
-                            <a class="dropdown-item" href="{{ url('/waypoints')}}">Turnpoint Database</a>
+                            <a class="dropdown-item" href="{{ url('/cups')}}">Download Waypoints</a>
+                            <a class="dropdown-item" href="{{ url('/waypoints')}}">Waypoint Database</a>
                         </div>
                     </li>
                     <li class="nav-messages"></li>
