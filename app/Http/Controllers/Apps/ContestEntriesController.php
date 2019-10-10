8a8fe2ae5c690aae943fd9ea35c9cb596433d2ff
Ivanda Nothabeer <kossaw.eve@gmail.com>
2019-10-10 16:10:36 +1300

    Email Confirmation for Contest Entries

--- app/Http/Controllers/Apps/ContestEntriesController.php
+++ app/Http/Controllers/Apps/ContestEntriesController.php
@@ -11,6 +11,7 @@ use App\Models\ContestEntry;
 use App\Grids\ContestentriesGrid;
 use App\Grids\ContestentriesGridInterface;
 use App\Models\Contest;
+use App\Models\ContestClass;
 use App\Models\ContestProfile;
 
 Use App\Http\Requests\ContestEntryRequest;
@@ -18,6 +19,7 @@ Use App\Http\Requests\ContestEntryRequest;
 use Form;
 use Gate;
 use Auth;
+use Mail;
 
 class ContestEntriesController extends Controller
 {
@@ -86,6 +88,7 @@ class ContestEntriesController extends Controller
         $contestEntry = ContestEntry::create($request->except(['_method','_token','contest_name']));
 
         if ($contestEntry->exists) {
+            $this->send_confirmation_mail($request);
             return redirect('contestentries');
         }
         return redirect(null,500);
@@ -98,6 +101,7 @@ class ContestEntriesController extends Controller
         $status = ContestEntry::where('id',$id)->update($request->except(['_method','_token','contest_name']));
 
         if ($status) {
+            $this->send_confirmation_mail($request);
             return response()->json(['success'=>true,'message'=>'Contest Entry Updated']);
         }
         return response()->json(['success' => false], 400);
@@ -126,5 +130,26 @@ class ContestEntriesController extends Controller
         $data = ContestProfile::find(auth()->user()->id);
         if ($data) $data = json_encode($data->toarray());
         return json_encode($data);
+    }
+
+    public function send_confirmation_mail($request)
+    {
+
+        $email = $request->email;
+        $contest = Contest::find($request->contest_id)->name;
+        $pilot = $request->first_name.' '.$request->last_name;
+        $className = ContestClass::find($request->classes_id)->name;
+        $aircraft = $request->glider;
+
+        Mail::send('emails.confirmentry',[
+            'contest' => $contest,
+            'pilot' => $pilot,
+            'className' => $className,
+            'aircraft' => $aircraft
+            ],
+            function ($m) use ($email, $contest) {
+                $m->from('mail@gliding.net.nz', 'Gliding New Zealand');
+                $m->to($email)->subject('Your Entry to '.$contest.' has been received');
+        });
     }
 }
