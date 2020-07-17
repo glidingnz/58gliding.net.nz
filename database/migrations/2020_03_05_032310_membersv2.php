94069ba1627c71308d8799fff6f1f9ef7df0867a
Tim Bromhead <github@pear.co.nz>
2020-07-17 14:14:47 +1200

    Updated org details in migration

--- database/migrations/2020_03_05_032310_membersv2.php
+++ database/migrations/2020_03_05_032310_membersv2.php
@@ -17,6 +17,189 @@ class Membersv2 extends Migration
 	 */
 	public function up()
 	{
+
+		// Add ability to identify defunct organisations
+		
+		Schema::table('orgs', function (Blueprint $table) {
+			$table->boolean('active')->default(true);
+			$table->string('twitter')->nullable();
+			$table->string('facebook')->nullable();
+			$table->integer('waypoint')->nullable();
+			$table->text('description')->nullable();
+		});
+
+		// insert defunct orgs
+		if (!Org::where('gnz_code','gsl')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Gliding South";
+			$neworg->short_name = "South";
+			$neworg->slug = "south";
+			$neworg->website = "";
+			$neworg->gnz_code = "gsl";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+
+		if (!Org::where('gnz_code','whg')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Whangarei";
+			$neworg->short_name = "Whangarei";
+			$neworg->slug = "whangarei";
+			$neworg->website = "";
+			$neworg->gnz_code = "whg";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','hac')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Hauraki Aero Club";
+			$neworg->short_name = "Hauraki";
+			$neworg->slug = "hauraki";
+			$neworg->website = "";
+			$neworg->gnz_code = "hac";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','wav')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Wigram Aviation Sports";
+			$neworg->short_name = "Wigram";
+			$neworg->slug = "wigram";
+			$neworg->website = "";
+			$neworg->gnz_code = "wav";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','not')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "North Otago";
+			$neworg->short_name = "North Otago";
+			$neworg->slug = "not";
+			$neworg->website = "";
+			$neworg->gnz_code = "not";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','otg')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Otago Gliding Club";
+			$neworg->short_name = "Otago Gliding Club";
+			$neworg->slug = "otg";
+			$neworg->website = "";
+			$neworg->gnz_code = "otg";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','clv')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Clutha Valley";
+			$neworg->short_name = "Clutha Valley";
+			$neworg->slug = "clutha";
+			$neworg->website = "";
+			$neworg->gnz_code = "clv";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','ssg')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Southern Soaring";
+			$neworg->short_name = "Southern Soaring";
+			$neworg->slug = "ssg";
+			$neworg->website = "";
+			$neworg->gnz_code = "ssg";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','ras')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Rangiora Advanced Soaring";
+			$neworg->short_name = "Rangiora Advanced Soaring";
+			$neworg->slug = "ras";
+			$neworg->website = "";
+			$neworg->gnz_code = "ras";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','alp')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Alpine Soaring";
+			$neworg->short_name = "Alpine Soaring";
+			$neworg->slug = "alp";
+			$neworg->website = "";
+			$neworg->gnz_code = "alp";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','rga')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Rangiora";
+			$neworg->short_name = "Rangiora";
+			$neworg->slug = "rangiora";
+			$neworg->website = "";
+			$neworg->gnz_code = "rga";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','rpu')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Ruapehu";
+			$neworg->short_name = "Ruapehu";
+			$neworg->slug = "ruapehu";
+			$neworg->website = "";
+			$neworg->gnz_code = "rpu";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','epb')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Eastern Bay of Plenty";
+			$neworg->short_name = "Eastern Bay of Plenty";
+			$neworg->slug = "epb";
+			$neworg->website = "";
+			$neworg->gnz_code = "epb";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+		if (!Org::where('gnz_code','sky')->exists())
+		{
+			$neworg = new Org;
+			$neworg->name = "Sky Sailing";
+			$neworg->short_name = "Sky Sailing";
+			$neworg->slug = "sky";
+			$neworg->website = "";
+			$neworg->gnz_code = "sky";
+			$neworg->type = "club";
+			$neworg->active = false;
+			$neworg->save();
+		}
+
+
 		// Table to link a member with an organisation, and track membership history
 		if (!Schema::hasTable('affiliates')) {
 			Schema::create('affiliates', function (Blueprint $table) {
@@ -29,6 +212,7 @@ class Membersv2 extends Migration
 				$table->text('resigned_comment')->nullable();
 				$table->boolean('resigned')->default(false);
 				$table->timestamps();
+				$table->index('member_id', 'member_index');
 			});
 		}
 
@@ -102,9 +286,6 @@ class Membersv2 extends Migration
 				$table->timestamps();
 			});
 		}
-
-
-
 
 		// Member Club Positions e.g. Secratary, CFI etc
 		if (!Schema::hasTable('positions')) {
@@ -197,6 +378,61 @@ class Membersv2 extends Migration
 				$prev_clubs = explode(' ', $member->previous_clubs);
 				foreach ($prev_clubs AS $prev_club)
 				{
+
+					// convert other names to the correct code
+					switch($prev_club)
+					{
+						case 'Tauranga Gliding Club': 
+						case 'Tauranga': 
+							$prev_club='TGA'; break;
+						case 'Auckland Gliding Club':
+						case 'Auckland':
+							$prev_club='AKL'; break;
+						case 'Wellington Gliding Club':
+						case 'Wellington':
+							$prev_club='WLN'; break;
+						case 'Piako Gliding Club':
+						case 'Piako':
+						case 'PkoPKO':
+							$prev_club='PKO'; break;
+						case 'South Canterbury Gliding Club':
+							$prev_club='SCY'; break;
+						case 'Nelson Lakes Gliding Club':
+							$prev_club='NLN'; break;
+						case 'Canterbury Gliding':
+							$prev_club='CTY'; break;
+						case 'Taupo Gliding Club':
+						case 'TpoTPO ':
+							$prev_club='TPO'; break;
+						case 'AklAKL  ':
+							$prev_club='AKL'; break;
+						case 'Omarama Gliding Club  ':
+							$prev_club='OGC'; break;
+						case 'GSLOTG':
+							$prev_club='GSL'; break;
+						case 'Waipukurau Gliding Club':
+						case 'Waipukurau Gliding ClubHBY':
+							$prev_club='GSL'; break;
+						case 'Marlborough Gliding Club':
+							$prev_club='MLB'; break;
+						case 'South Canterbury Gliding Clu':
+							$prev_club='SCY'; break;
+						case 'GomGOM ':
+							$prev_club='GOM'; break;
+						case 'Gliding Manawatu':
+							$prev_club='WGM'; break;
+						case 'Gliding Wairarapa':
+							$prev_club='GWR'; break;
+						case 'Hawkes Bay Gliding Club':
+						case 'Waipukurau Gliding Club/ Hawkes Bay Gliding Club':
+							$prev_club='HBY'; break;
+						case 'Auckland Aviation Sports Club':
+							$prev_club='AAV'; break;
+						case 'Norfolk Aviation Sports Club':
+							$prev_club='AAV'; break;
+					}
+
+
 					if (isset($orgs_gnz_code[$prev_club]))
 					{
 						$affiliate = new Affiliate();
@@ -250,5 +486,30 @@ class Membersv2 extends Migration
 		Schema::drop('member_membertype');
 		Schema::drop('positions');
 		Schema::drop('member_position');
+		
+		Org::where('gnz_code','gsl')->delete();
+		Org::where('gnz_code','whg')->delete();
+		Org::where('gnz_code','hac')->delete();
+		Org::where('gnz_code','wav')->delete();
+		Org::where('gnz_code','not')->delete();
+		Org::where('gnz_code','otg')->delete();
+		Org::where('gnz_code','clv')->delete();
+		Org::where('gnz_code','ssg')->delete();
+		Org::where('gnz_code','ras')->delete();
+		Org::where('gnz_code','alp')->delete();
+		Org::where('gnz_code','rga')->delete();
+		Org::where('gnz_code','rpu')->delete();
+		Org::where('gnz_code','epb')->delete();
+		Org::where('gnz_code','sky')->delete();
+
+
+		Schema::table('orgs', function (Blueprint $table) {
+			$table->dropColumn('active');
+			$table->dropColumn('twitter');
+			$table->dropColumn('facebook');
+			$table->dropColumn('waypoint');
+			$table->dropColumn('description');
+		});
+
 	}
 }
